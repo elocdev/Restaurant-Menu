645519fc0af2dd314107a1e3582ef1db4545c656
Eric Loc <eloc1385@gmail.com>
2022-06-16 07:39:05 -0700

    started to add descriptions to menu items

--- scripts/script.js
+++ scripts/script.js
@@ -15,7 +15,7 @@ function openMenuSection(evt, menuItem) {
     }
 
     // Show the current tab, and add an "active" class to the link that opened the tab
-    document.getElementById(menuItem).style.display = "block";
+    document.getElementById(menuItem).style.display = "flex";
     evt.currentTarget.className += " active";
 }
 
